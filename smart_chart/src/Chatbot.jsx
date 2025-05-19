aef5e6887f40d5f0a296b6406124a9d296fa6aef
Jibex-Banks <mudathirajibolamudathir@gmail.com>
2025-05-19 23:49:53 +0100

    making the the chat interface functional without the mic though

--- smart_chart/src/Chatbot.jsx
+++ smart_chart/src/Chatbot.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Chatbot.css';
 import { Circle, Mic, Send, WifiZero } from 'lucide-react';
 import react from "./assets/react.svg";
 
 function Chatbot() {
+    const [messages, setMessages] = useState([
+        {'id' : 1, "message" : "Hello!, How can I help you today?", 'sender':"bot"},
+    ]);
+
+    const [inputValue, setInputValue] = useState("");
+
+    function sendMessage(){
+        if(inputValue.trim() === "")
+            return;
+
+       const newMessages = ([
+        ...messages,
+        {'id': messages.length + 1, "message": inputValue, "sender":"user"},
+       ]);
+       setMessages(newMessages);
+       setInputValue("");
+
+       setTimeout(() => {
+        setMessages([
+            ...newMessages,
+            {'id': messages.length + 1, "message": "I received your message. This is a simulated response.", "sender":"bot"},
+        ]);
+        
+       }, 1000);
+    }
+
+    const HandlekeyPressed = (e) => {
+        if(e.key === 'Enter'){
+            sendMessage();
+        }
+    }
+
     return (
         <>
             <div className="logo">
@@ -18,11 +50,23 @@ function Chatbot() {
                         <div className="status"><div className="dot"></div> Online</div>
                     </div>
                 </div>
-                <div className="chat_container"></div>
+                <div className="chat_container">
+                    {messages.map((message,id) => (
+                        <div key={id} className={message.sender}>
+                        <p>{message.message}</p>
+                    </div>
+                    ))}
+                    {/* <div className="bot">
+                        <p>Hello!, How can I help you today?</p>
+                    </div>
+                    <div className="user">
+                        <p>Hello!, How can I help you today?</p>
+                    </div> */}
+                </div>
                 <div className="chat_bottom">
                     <button className="mic_btn"><Mic/></button>
-                    <input type="text" name="" id="" placeholder='Type your message here...'/>
-                    <button className='send_btn'> <Send/> </button>
+                    <input type="text" name="" id="" className="user_input" placeholder='Type your message here...' onChange={(e)=>{setInputValue(e.target.value)}} value={inputValue} onKeyDown={HandlekeyPressed}/>
+                    <button className='send_btn' onClick={sendMessage}> <Send/> </button>
                 </div>
             </div>
         </>
